54ec5d46a218af7c7ccbd4f3a65c77c21129a126
Nikos <nikos.sklikas@canonical.com>
2022-10-21 18:56:15 +0300

    Fix logic

--- src/charm.py
+++ src/charm.py
@@ -9,7 +9,7 @@ import logging
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import BlockedStatus, WaitingStatus
+from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
 
 logger = logging.getLogger(__name__)
 
@@ -24,8 +24,8 @@ class KratosIdpIntegratorCharm(CharmBase):
     """Charm the service."""
 
     _stored = StoredState()
-    _RELATION_NAME = "provider_endpoint"
-    _ALLOWED_PROVIDERS = [
+    _relation_name = "provider-endpoint"
+    _allowed_providers = [
         "generic",
         "google",
         "facebook",
@@ -43,19 +43,26 @@ class KratosIdpIntegratorCharm(CharmBase):
         "vkontakte",
         "dingtalk",
     ]
+    provider_extra_config = {
+        "microsoft": ["microsoft_tenant_id"],
+        "apple": ["apple_team_id", "apple_private_key_id", "apple_private_key"],
+    }
+    optional_config = ["requested_claims", "issuer_url"]
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.config_changed, self._configure_relation)
-        self.framework.observe(
-            self.on.provider_endpoint_relation_created, self._configure_relation
-        )
-        self.framework.observe(
-            self.on.provider_endpoint_relation_created, self._configure_relation
-        )
-        self.framework.observe(self.on.provider_endpoint_relation_changed, self._relation_changed)
 
-        # Actions
+        # Charm events
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.update_status, self._on_update_status)
+
+        # Relation events
+        self.framework.observe(
+            self.on.provider_endpoint_relation_created, self._on_relation_created
+        )
+        self.framework.observe(self.on.provider_endpoint_relation_changed, self._on_relation_changed)
+
+        # Action events
         self.framework.observe(self.on.get_redirect_uri_action, self._get_redirect_uri)
         self.framework.observe(self.on.get_logo_action, self._get_logo)
         self.framework.observe(self.on.set_logo_action, self._set_logo)
@@ -73,71 +80,112 @@ class KratosIdpIntegratorCharm(CharmBase):
     @property
     def _relation(self):
         try:
-            _relation = self.model.get_relation(self._RELATION_NAME)
+            _relation = self.model.get_relation(self._relation_name)
         except KeyError:
-            self.unit.status = WaitingStatus(f"Relation '{self._RELATION_NAME}' does not exist")
             return None
         return _relation
+
+    def _validate_provider_extra_fields(self):
+        provider = self.config["provider"]
+        for field in self.provider_extra_config.get(provider, []):
+            if not self.config.get(field):
+                raise InvalidConfigError(
+                    f"Missing required configuration for {provider}: '{field}'"
+                )
+
+    def _get_provider_extra_fields(self):
+        return {
+            field: self.config[field]
+            for field in self.provider_extra_config.get(self.config["provider"], [])
+        }
 
     def _validate_config(self):
         """Validate the charm config.
 
-        Validate that:
+        Validates that:
         - client_id exists and it is not None
         - client_secret exists and it is not None
         - provider is valid
+        - provider specific configurations are set
         """
         if not self.config.get("client_id"):
             raise InvalidConfigError("Missing required configuration: 'client_id'")
         elif not self.config.get("client_secret"):
             raise InvalidConfigError("Missing required configuration: 'client_secret'")
-        elif self.config["provider"] not in self._ALLOWED_PROVIDERS:
+        elif self.config["provider"] not in self._allowed_providers:
             raise InvalidConfigError(
                 "Required configuration 'provider' MUST be one of the following: "
-                ", ".join(self._ALLOWED_PROVIDERS)
+                ", ".join(self._allowed_providers)
             )
-        elif self.config["provider"] == "generic" and not self.config["issuer_url"]:
-            raise InvalidConfigError("issuer_url MUST be provided for 'generic' providers")
-
-        if self.config["provider"] != "generic" and self.config["issuer_url"] is not None:
-            # Should we raise here?
-            logger.warn(
-                "'issuer_url' is provided, but provider is not 'generic'. "
-                "'issuer_url' will be ignored"
+        elif self.config["provider"] in ["generic", "auth0"] and "issuer_url" not in self.config:
+            raise InvalidConfigError(
+                "issuer_url MUST be provided for 'generic' and 'auth0' providers"
             )
 
-    def _configure_relation(self, event):
+        # Should we also validate that the user didn't provide any unused config?
+        # E.g. microsoft_tenant_id with provider=google
+
+        self._validate_provider_extra_fields()
+
+    def _get_optional_config(self):
+        return {
+            field: self.config[field] for field in self.optional_config if self.config.get(field)
+        }
+
+    def _configure_relation(self, relation):
         """Validate that the config is valid and pass it to the relation."""
-        relation = getattr(event, "relation", None) or self._relation
+        if not relation or isinstance(self.unit.status, BlockedStatus):
+            return
 
-        # We validate the config first as we don't
+        data = {
+            "client_id": self.config["client_id"],
+            # TODO: Use juju secret to transmit client_secret once the ops library supports it.
+            # Relevant PR: https://github.com/canonical/operator/pull/840
+            # TODO: Use vault
+            "client_secret": self.config["client_secret"],
+            "provider": self.config["provider"],
+            **self._get_optional_config(),
+            **self._get_provider_extra_fields(),
+        }
+
+        relation.data[self.app].update(data)
+
+    def _on_relation_created(self, event):
+        relation = event.relation
+        self._configure_relation(relation)
+        self._on_update_status(event)
+
+    def _on_relation_changed(self, event):
+        relation = event.relation
+        self._stored.redirect_uri = relation.data[event.app].get("redirect_uri")
+        self._on_update_status(event)
+
+    def _on_config_changed(self, event):
+        self.unit.status = WaitingStatus("Configuring the charm")
         try:
             self._validate_config()
         except InvalidConfigError as e:
             self.unit.status = BlockedStatus(e.args[0])
             # How should we handle invalid config?
-            if relation:
-                relation.data[self.app].clear()
+            # if self.relation:
+            #     self.relation.data[self.app].clear()
             return
+        self._configure_relation(self._relation)
+        self._on_update_status(event)
 
-        if not relation:
-            return
-
-        data = {
-            "client_id": self.config["client_id"],
-            # TODO: Use juju secret to transmit this once the ops library supports it.
-            # Relevant PR: https://github.com/canonical/operator/pull/840
-            "client_secret": self.config["client_secret"],
-            "provider": self.config["provider"],
-            "issuer_url": self.config["issuer_url"],
-        }
-
-        relation.data[self.app].update(data)
-
-    def _relation_changed(self, event):
-        relation = event.relation
-        self._stored.redirect_uri = relation.data[event.app].get("redirect_uri")
-        self._configure_relation(event)
+    def _on_update_status(self, event):
+        """Set the unit status
+        - If unit is blocked, leave it that way (it means that mandatory config is missing)
+        - If no relation exists, status is waiting
+        - Else status is active
+        """
+        if not isinstance(self.unit.status, BlockedStatus):
+            if not self._relation:
+                self.unit.status = WaitingStatus("Waiting for relation with Kratos")
+            elif not self._stored.redirect_uri:
+                self.unit.status = WaitingStatus("Waiting for Kratos to register provider")
+            else:
+                self.unit.status = ActiveStatus()
 
     def _get_redirect_uri(self, event):
         """Get the redirect_uri from the relation and return it to the user."""
