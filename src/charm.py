5e0dcd7e8723a450731ca015d66803c59c6b908c
Nikos <nikos.sklikas@canonical.com>
2022-11-11 15:43:02 +0200

    Refactor logic into library

--- src/charm.py
+++ src/charm.py
@@ -6,6 +6,7 @@
 
 import logging
 
+from charms.kratos_external_idp_integrator.v0.kratos_external_provider import ExternalIdpProvider
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
@@ -14,167 +15,49 @@ from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
 logger = logging.getLogger(__name__)
 
 
-class InvalidConfigError(Exception):
-    """Internal exception that is raised if the charm config is not valid."""
-
-    pass
-
-
 class KratosIdpIntegratorCharm(CharmBase):
     """Charm the service."""
 
     _stored = StoredState()
     _relation_name = "provider-endpoint"
-    _allowed_providers = [
-        "generic",
-        "google",
-        "facebook",
-        "microsoft",
-        "github",
-        "apple",
-        "gitlab",
-        "auth0",
-        "slack",
-        "spotify",
-        "discord",
-        "twitch",
-        "netid",
-        "yandex",
-        "vkontakte",
-        "dingtalk",
-    ]
-    provider_extra_config = {
-        "microsoft": ["microsoft_tenant_id"],
-        "apple": ["apple_team_id", "apple_private_key_id", "apple_private_key"],
-    }
-    optional_config = ["requested_claims", "issuer_url"]
 
     def __init__(self, *args):
         super().__init__(*args)
+        self.external_idp_provider = ExternalIdpProvider(self, self.config)
 
         # Charm events
         self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.framework.observe(self.on.update_status, self._on_update_status)
 
-        # Relation events
+        # Library events
         self.framework.observe(
-            self.on.provider_endpoint_relation_created, self._on_relation_created
+            self.external_idp_provider.on.redirect_uri_changed, self._on_redirect_uri_changed
         )
-        self.framework.observe(self.on.provider_endpoint_relation_changed, self._on_relation_changed)
+        self.framework.observe(
+            self.external_idp_provider.on.invalid_client_config, self._on_invalid_client_config
+        )
 
         # Action events
         self.framework.observe(self.on.get_redirect_uri_action, self._get_redirect_uri)
-        self.framework.observe(self.on.get_logo_action, self._get_logo)
-        self.framework.observe(self.on.set_logo_action, self._set_logo)
-        self.framework.observe(self.on.get_identity_schema_action, self._get_identity_schema)
-        self.framework.observe(self.on.set_identity_schema_action, self._set_identity_schema)
-        self.framework.observe(self.on.get_scopes_action, self._get_scopes)
-        self.framework.observe(self.on.set_scopes_action, self._set_scopes)
-        self.framework.observe(self.on.get_provider_name_action, self._get_provider_name)
-        self.framework.observe(self.on.set_provider_name_action, self._set_provider_name)
         self.framework.observe(self.on.enable_action, self._enable)
         self.framework.observe(self.on.disable_action, self._disable)
 
         self._stored.set_default(redirect_uri="")
+        self._stored.set_default(enabled=True)
 
     @property
     def _relation(self):
-        try:
-            _relation = self.model.get_relation(self._relation_name)
-        except KeyError:
-            return None
-        return _relation
-
-    def _validate_provider_extra_fields(self):
-        provider = self.config["provider"]
-        for field in self.provider_extra_config.get(provider, []):
-            if not self.config.get(field):
-                raise InvalidConfigError(
-                    f"Missing required configuration for {provider}: '{field}'"
-                )
-
-    def _get_provider_extra_fields(self):
-        return {
-            field: self.config[field]
-            for field in self.provider_extra_config.get(self.config["provider"], [])
-        }
-
-    def _validate_config(self):
-        """Validate the charm config.
-
-        Validates that:
-        - client_id exists and it is not None
-        - client_secret exists and it is not None
-        - provider is valid
-        - provider specific configurations are set
-        """
-        if not self.config.get("client_id"):
-            raise InvalidConfigError("Missing required configuration: 'client_id'")
-        elif not self.config.get("client_secret"):
-            raise InvalidConfigError("Missing required configuration: 'client_secret'")
-        elif self.config["provider"] not in self._allowed_providers:
-            raise InvalidConfigError(
-                "Required configuration 'provider' MUST be one of the following: "
-                ", ".join(self._allowed_providers)
-            )
-        elif self.config["provider"] in ["generic", "auth0"] and "issuer_url" not in self.config:
-            raise InvalidConfigError(
-                "issuer_url MUST be provided for 'generic' and 'auth0' providers"
-            )
-
-        # Should we also validate that the user didn't provide any unused config?
-        # E.g. microsoft_tenant_id with provider=google
-
-        self._validate_provider_extra_fields()
-
-    def _get_optional_config(self):
-        return {
-            field: self.config[field] for field in self.optional_config if self.config.get(field)
-        }
-
-    def _configure_relation(self, relation):
-        """Validate that the config is valid and pass it to the relation."""
-        if not relation or isinstance(self.unit.status, BlockedStatus):
-            return
-
-        data = {
-            "client_id": self.config["client_id"],
-            # TODO: Use juju secret to transmit client_secret once the ops library supports it.
-            # Relevant PR: https://github.com/canonical/operator/pull/840
-            # TODO: Use vault
-            "client_secret": self.config["client_secret"],
-            "provider": self.config["provider"],
-            **self._get_optional_config(),
-            **self._get_provider_extra_fields(),
-        }
-
-        relation.data[self.app].update(data)
-
-    def _on_relation_created(self, event):
-        relation = event.relation
-        self._configure_relation(relation)
-        self._on_update_status(event)
-
-    def _on_relation_changed(self, event):
-        relation = event.relation
-        self._stored.redirect_uri = relation.data[event.app].get("redirect_uri")
-        self._on_update_status(event)
+        return self.model.get_relation(self._relation_name)
 
     def _on_config_changed(self, event):
         self.unit.status = WaitingStatus("Configuring the charm")
-        try:
-            self._validate_config()
-        except InvalidConfigError as e:
-            self.unit.status = BlockedStatus(e.args[0])
-            # How should we handle invalid config?
-            # if self.relation:
-            #     self.relation.data[self.app].clear()
-            return
-        self._configure_relation(self._relation)
+        self.external_idp_provider.update_client_config(self.config)
+        self._configure_relation()
         self._on_update_status(event)
 
     def _on_update_status(self, event):
-        """Set the unit status
+        """Set the unit status.
+
         - If unit is blocked, leave it that way (it means that mandatory config is missing)
         - If no relation exists, status is waiting
         - Else status is active
@@ -182,59 +65,47 @@ class KratosIdpIntegratorCharm(CharmBase):
         if not isinstance(self.unit.status, BlockedStatus):
             if not self._relation:
                 self.unit.status = WaitingStatus("Waiting for relation with Kratos")
-            elif not self._stored.redirect_uri:
+            elif not self._stored.redirect_uri and self._stored.enabled:
                 self.unit.status = WaitingStatus("Waiting for Kratos to register provider")
             else:
                 self.unit.status = ActiveStatus()
 
+    def _on_redirect_uri_changed(self, event):
+        self._stored.redirect_uri = event.redirect_uri
+        self._on_update_status(event)
+
+    def _on_invalid_client_config(self, event):
+        # Can this be fired before the
+        self.unit.status = BlockedStatus(event.error)
+        # TODO: Do we need to delete the client?
+        self.external_idp_provider.remove_client()
+
     def _get_redirect_uri(self, event):
         """Get the redirect_uri from the relation and return it to the user."""
-        redirect_uri = self._stored.redirect_uri
-        if not redirect_uri:
-            # Perhaps a more descriptive message is needed here?
-            event.set_results("No redirect_uri found")
+        if redirect_uri := self._stored.redirect_uri:
+            event.set_results({"redirect_uri": redirect_uri})
         else:
-            event.set_results({"redirect_uri": self._stored.redirect_uri})
-
-    def _get_logo(self, event):
-        # TODO: Implement once we agree on a provider management API
-        raise NotImplementedError()
-
-    def _set_logo(self, event):
-        # TODO: Implement once we agree on a provider management API
-        raise NotImplementedError()
-
-    def _get_identity_schema(self, event):
-        # TODO: Implement once we agree on a provider management API
-        raise NotImplementedError()
-
-    def _set_identity_schema(self, event):
-        # TODO: Implement once we agree on a provider management API
-        raise NotImplementedError()
-
-    def _get_scopes(self, event):
-        # TODO: Implement once we agree on a provider management API
-        raise NotImplementedError()
-
-    def _set_scopes(self, event):
-        # TODO: Implement once we agree on a provider management API
-        raise NotImplementedError()
-
-    def _get_provider_name(self, event):
-        # TODO: Implement once we agree on a provider management API
-        raise NotImplementedError()
-
-    def _set_provider_name(self, event):
-        # TODO: Implement once we agree on a provider management API
-        raise NotImplementedError()
+            # More descriptive message is needed?
+            event.set_results("No redirect_uri found")
 
     def _enable(self, event):
-        # TODO: Implement once we agree on a provider management API
-        raise NotImplementedError()
+        self._stored.enabled = True
+        self._configure_relation()
 
     def _disable(self, event):
-        # TODO: Implement once we agree on a provider management API
-        raise NotImplementedError()
+        self._stored.enabled = False
+        self._configure_relation()
+
+    def _configure_relation(self):
+        """Validate that the config is valid and pass it to the relation."""
+        if not self._relation or isinstance(self.unit.status, BlockedStatus):
+            return
+
+        if not self._stored.enabled:
+            self.external_idp_provider.remove_client()
+            return
+
+        self.external_idp_provider.create_client()
 
 
 if __name__ == "__main__":
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -1,12 +1,13 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import json
 from unittest.mock import MagicMock
 
 import ops
 import pytest
-from ops.testing import Harness
 from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
+from ops.testing import Harness
 
 from charm import KratosIdpIntegratorCharm
 
@@ -20,6 +21,20 @@ def config():
         "client_secret": "client_secret",
         "provider": "generic",
         "issuer_url": "http://example.com",
+        "secret_backend": "plain",
+    }
+
+
+@pytest.fixture
+def generic_databag(config):
+    return {
+        "client_id": config["client_id"],
+        "provider": config["provider"],
+        "secret_backend": config["secret_backend"],
+        config["provider"]: {
+            "client_secret": config["client_secret"],
+            "issuer_url": config["issuer_url"],
+        },
     }
 
 
@@ -30,6 +45,7 @@ def microsoft_config():
         "client_secret": "client_secret",
         "provider": "microsoft",
         "microsoft_tenant_id": "some-tenant-id",
+        "secret_backend": "plain",
     }
 
 
@@ -37,8 +53,8 @@ def microsoft_config():
 def apple_config():
     return {
         "client_id": "client_id",
-        "client_secret": "client_secret",
         "provider": "apple",
+        "secret_backend": "plain",
         "apple_team_id": "apple_team_id",
         "apple_private_key_id": "apple_private_key_id",
         "apple_private_key": "apple_private_key",
@@ -51,6 +67,7 @@ def invalid_provider_config():
         "client_id": "client_id",
         "client_secret": "client_secret",
         "provider": "go0gle",
+        "secret_backend": "plain",
     }
 
 
@@ -63,7 +80,10 @@ def mock_event():
 
 @pytest.fixture
 def relation_data():
-    return {"redirect_uri": "https://example.com/callback"}
+    return {
+        "redirect_uri": "https://example.com/callback",
+        "provider_id": "provider",
+    }
 
 
 @pytest.fixture
@@ -75,23 +95,73 @@ def harness():
     harness.cleanup()
 
 
-def test_relation(harness, config, relation_data):
+def test_relation(harness, config, relation_data, generic_databag):
+    provider = config["provider"]
+
     harness.update_config(config)
     relation_id = harness.add_relation("provider-endpoint", "kratos-app")
     harness.update_relation_data(relation_id, "kratos-app", relation_data)
 
     unit_data = harness.get_relation_data(relation_id, harness.charm.unit)
     app_data = harness.get_relation_data(relation_id, harness.charm.app)
+    app_data[provider] = json.loads(app_data[provider])
 
     assert isinstance(harness.charm.unit.status, ActiveStatus)
     assert unit_data == {}
-    assert app_data == config
+    assert app_data == generic_databag
 
 
-def test_invalid_config(harness, invalid_provider_config, relation_data):
-    harness.update_config(invalid_provider_config, relation_data)
+def test_extra_config(harness, config, relation_data, generic_databag, caplog):
+    provider = config["provider"]
+    config["microsoft_tenant_id"] = "4242424242"
+
+    harness.update_config(config)
     relation_id = harness.add_relation("provider-endpoint", "kratos-app")
     harness.update_relation_data(relation_id, "kratos-app", relation_data)
+
+    unit_data = harness.get_relation_data(relation_id, harness.charm.unit)
+    app_data = harness.get_relation_data(relation_id, harness.charm.app)
+    app_data[provider] = json.loads(app_data[provider])
+
+    assert isinstance(harness.charm.unit.status, ActiveStatus)
+    assert (
+        caplog.records[0].message
+        == "Invalid config 'microsoft_tenant_id' for provider 'generic' will be ignored"
+    )
+    assert app_data == generic_databag
+
+
+def test_config_no_relation(harness, config, relation_data):
+    harness.update_config(config)
+    assert isinstance(harness.charm.unit.status, WaitingStatus)
+
+
+def test_invalid_config(harness, invalid_provider_config, config, generic_databag, relation_data):
+    harness.update_config(invalid_provider_config)
+    relation_id = harness.add_relation("provider-endpoint", "kratos-app")
+
+    unit_data = harness.get_relation_data(relation_id, harness.charm.unit)
+    app_data = harness.get_relation_data(relation_id, harness.charm.app)
+    assert isinstance(harness.charm.unit.status, BlockedStatus)
+    assert unit_data == {}
+    assert app_data == {}
+
+    harness.update_config(config)
+    harness.update_relation_data(relation_id, "kratos-app", relation_data)
+
+    provider = config["provider"]
+    app_data = harness.get_relation_data(relation_id, harness.charm.app)
+    app_data[provider] = json.loads(app_data[provider])
+
+    assert isinstance(harness.charm.unit.status, ActiveStatus)
+    assert app_data == generic_databag
+
+
+def test_invalid_provider(harness, config, relation_data):
+    config["provider"] = "error"
+    harness.update_config(config)
+    relation_id = harness.add_relation("provider-endpoint", "kratos-app")
+    # harness.update_relation_data(relation_id, "kratos-app", relation_data)
 
     unit_data = harness.get_relation_data(relation_id, harness.charm.unit)
     app_data = harness.get_relation_data(relation_id, harness.charm.app)
@@ -101,12 +171,25 @@ def test_invalid_config(harness, invalid_provider_config, relation_data):
 
 
 def test_microsoft_config(harness, microsoft_config, relation_data):
+    expected_databag = {
+        "client_id": microsoft_config["client_id"],
+        "provider": microsoft_config["provider"],
+        "secret_backend": microsoft_config["secret_backend"],
+        "microsoft": {
+            "tenant_id": microsoft_config["microsoft_tenant_id"],
+            "client_secret": microsoft_config["client_secret"],
+        },
+    }
+
     harness.update_config(microsoft_config)
     relation_id = harness.add_relation("provider-endpoint", "kratos-app")
     harness.update_relation_data(relation_id, "kratos-app", relation_data)
 
     app_data = harness.get_relation_data(relation_id, harness.charm.app)
-    assert app_data == microsoft_config
+    app_data["microsoft"] = json.loads(app_data["microsoft"])
+
+    assert isinstance(harness.charm.unit.status, ActiveStatus)
+    assert app_data == expected_databag
 
 
 def test_microsoft_invalid_config(harness, config, relation_data):
@@ -116,16 +199,31 @@ def test_microsoft_invalid_config(harness, config, relation_data):
     harness.update_relation_data(relation_id, "kratos-app", relation_data)
 
     app_data = harness.get_relation_data(relation_id, harness.charm.app)
+    assert isinstance(harness.charm.unit.status, BlockedStatus)
     assert app_data == {}
 
 
 def test_apple_config(harness, apple_config, relation_data):
+    expected_databag = {
+        "client_id": "client_id",
+        "provider": "apple",
+        "secret_backend": "plain",
+        "apple": {
+            "team_id": "apple_team_id",
+            "private_key_id": "apple_private_key_id",
+            "private_key": "apple_private_key",
+        },
+    }
+
     harness.update_config(apple_config)
     relation_id = harness.add_relation("provider-endpoint", "kratos-app")
     harness.update_relation_data(relation_id, "kratos-app", relation_data)
 
     app_data = harness.get_relation_data(relation_id, harness.charm.app)
-    assert app_data == apple_config
+    app_data["apple"] = json.loads(app_data["apple"])
+
+    assert isinstance(harness.charm.unit.status, ActiveStatus)
+    assert app_data == expected_databag
 
 
 def test_apple_invalid_config(harness, config, relation_data):
@@ -135,6 +233,7 @@ def test_apple_invalid_config(harness, config, relation_data):
     harness.update_relation_data(relation_id, "kratos-app", relation_data)
 
     app_data = harness.get_relation_data(relation_id, harness.charm.app)
+    assert isinstance(harness.charm.unit.status, BlockedStatus)
     assert app_data == {}
 
 
@@ -146,4 +245,35 @@ def test_get_redirect_uri(harness, config, mock_event, relation_data):
     harness.charm._get_redirect_uri(mock_event)
 
     mock_event.set_results.assert_called_once()
-    assert mock_event.set_results.mock_calls[0].args == (relation_data,)
+    assert isinstance(harness.charm.unit.status, ActiveStatus)
+    assert mock_event.set_results.mock_calls[0].args == (
+        dict(redirect_uri=relation_data["redirect_uri"]),
+    )
+
+
+def test_get_no_redirect_uri(harness, config, mock_event, relation_data):
+    harness.update_config(config)
+
+    harness.charm._get_redirect_uri(mock_event)
+
+    mock_event.set_results.assert_called_once()
+    assert mock_event.set_results.mock_calls[0].args == ("No redirect_uri found",)
+
+
+def test_disable(harness, config, generic_databag, relation_data):
+    harness.update_config(config)
+    relation_id = harness.add_relation("provider-endpoint", "kratos-app")
+    harness.update_relation_data(relation_id, "kratos-app", relation_data)
+
+    harness.charm._disable(mock_event)
+
+    app_data = harness.get_relation_data(relation_id, harness.charm.app)
+    assert isinstance(harness.charm.unit.status, ActiveStatus)
+    assert app_data == {}
+
+    harness.charm._enable(mock_event)
+    provider = config["provider"]
+    app_data[provider] = json.loads(app_data[provider])
+
+    assert isinstance(harness.charm.unit.status, ActiveStatus)
+    assert app_data == generic_databag
