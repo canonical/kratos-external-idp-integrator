ee7c2305072c597488cbdef2680661feeb40f18d
dushu <shu.du@canonical.com>
2025-03-22 18:02:20 -0400

    feat: add terraform module

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+/**
+ * # Terraform Module for Kratos External IDP Integrator Operator
+ *
+ * This is a Terraform module facilitating the deployment of the
+ * kratos-external-idp-integrator charm using the Juju Terraform provider.
+ */
+
+resource "juju_application" "kratos-external-idp" {
+  name        = var.app_name
+  model       = var.model_name
+  trust       = true
+  config      = var.config
+  constraints = var.constraints
+  units       = var.units
+
+  charm {
+    name     = "kratos-external-idp-integrator"
+    base     = var.base
+    channel  = var.channel
+    revision = var.revision
+  }
+}
